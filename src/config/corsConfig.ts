8cffd2dc5c227ca6b5d7364f5e1efceabf6cad32
Samarpan Harit <samarpan.harit@unicommerce.com>
2021-12-26 00:05:50 +0530

    Added heroku deployed url to cors origin list

--- src/config/corsConfig.ts
+++ src/config/corsConfig.ts
@@ -1,6 +1,13 @@
 import { CorsOptions } from "cors";
+const whitelist = ["https://localhost", "https://sup-gg.herokuapp.com"];
 
-export const corsOptions: CorsOptions = {
-    origin: "https://localhost",
+export const corsOptions:CorsOptions = {
+    origin: function (origin: any, callback: any) {
+        if (whitelist.indexOf(origin) !== -1 || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
     credentials: true,
 };
