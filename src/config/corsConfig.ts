fb22f3bbbf4aecf79b6b79d39684f1bb1488502b
Samarpan Harit <samarpan.harit@unicommerce.com>
2021-12-26 18:55:30 +0530

    Debug : Added logs for origin check

--- src/config/corsConfig.ts
+++ src/config/corsConfig.ts
@@ -3,6 +3,7 @@ const whitelist = ["http://localhost:3000", "https://sup-gg.netlify.app"];
 
 export const corsOptions:CorsOptions = {
     origin: function (origin: any, callback: any) {
+        console.log("origin : ",origin,", whitelist.indexOf(origin)",whitelist.indexOf(origin))
         if (whitelist.indexOf(origin) !== -1 || !origin) {
             callback(null, true);
         } else {
