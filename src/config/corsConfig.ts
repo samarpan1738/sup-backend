cf0840e35b20dd82e878d56e210c78dea7e652cf
Samarpan Harit <samarpan.harit@unicommerce.com>
2022-04-28 22:04:36 +0530

    Removed database config file

--- src/config/corsConfig.ts
+++ src/config/corsConfig.ts
@@ -1,7 +1,7 @@
 import { CorsOptions } from "cors";
 const corsOrigin: any =
     process.env.NODE_ENV === "development" ? process.env.CORS_ORIGIN_DEV : process.env.CORS_ORIGIN_PROD;
-export const corsOptions:CorsOptions = {
-    origin:corsOrigin ,
+export const corsOptions: CorsOptions = {
+    origin: corsOrigin,
     credentials: true,
 };
--- src/config/databaseConfig.ts
+++
@@ -1,6 +0,0 @@
-const dbUrl: any =
-    process.env.NODE_ENV === "development" ? process.env.DATABASE_URL_DEV : process.env.DATABASE_URL_PROD;
-export const corsOptions:CorsOptions = {
-    origin:corsOrigin ,
-    credentials: true,
-};
